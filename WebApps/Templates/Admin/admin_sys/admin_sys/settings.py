3ac4c974b5cdf7f93c76c17b3971463fb43ffa02
ola <ola.k.contact@gmail.com>
2025-05-29 16:06:58 +0100

    jazzadmin frontend

--- WebApps/Templates/Admin/admin_sys/admin_sys/settings.py
+++ WebApps/Templates/Admin/admin_sys/admin_sys/settings.py
@@ -137,3 +137,44 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#CHANGE THE TITLES IN JAZZADMIN
+JAZZMIN_SETTINGS = {
+    #"site_logo": "static/img/logo.png",
+    "welcome_sign": "Welcome to admin!",
+    "copyright": "Admin Ltd",
+    "show_ui_builder": True,
+
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": False,
+    "accent": "accent-primary",
+    "navbar": "navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-primary",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "cyborg",
+    "dark_mode_theme": "darkly",
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
+}
